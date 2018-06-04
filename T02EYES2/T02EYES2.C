c49e93fe02020e2cea6eddb3a3eb3aac4f74ada9
Dmitriy Ivanov (DI6) <akolyx@gmail.com>
2018-06-04 10:34:28 +0400

    Three modes of eyes with double buffering

--- T02EYES2/T02EYES2.C
+++ T02EYES2/T02EYES2.C
@@ -57,8 +57,8 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
   HDC hDC;
   PAINTSTRUCT ps;
 
-  static INT w, h, r = 50;
-  static INT mode = 1;
+  static INT w, h, rad = 50;
+  static INT mode = 0;
   static HDC hMemDC;
   static HBITMAP hBm;
 
@@ -87,26 +87,33 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
     return 0;
   case WM_KEYDOWN:
     if (wParam == VK_SPACE)
-      mode = 1 - mode;
+      mode = (mode + 1) % 3;
     return 0;
   case WM_TIMER:
+
+    InvalidateRect(hWnd, NULL, TRUE);
+
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+
     GetCursorPos(&pt);
     ScreenToClient(hWnd, &pt);
     
     SetDCBrushColor(hMemDC, RGB(100, 0, 0));
     Rectangle(hMemDC, 0, 0, w, h);
 
-    if (mode == 2)
+    if (mode == 0)
     {
-      r = w > h ? h : w;
-      DrawEye(hMemDC, w / 2 - r / 4 - r / 20, h / 2, r / 4, r / 20, pt.x, pt.y);
-      DrawEye(hMemDC, w / 2 + r/ 4 + r / 20, h / 2, r / 4, r / 20, pt.x, pt.y);
+      rad = w > h ? h : w;
+      DrawEye(hMemDC, w / 2 - rad / 4 - rad / 20, h / 2, rad / 4, rad / 20, pt.x, pt.y);
+      DrawEye(hMemDC, w / 2 + rad / 4 + rad / 20, h / 2, rad / 4, rad / 20, pt.x, pt.y);
     }
-    else if (mode == 0)
+    else if (mode == 1)
     {
       INT i;
 
-      srand(1244);        
+      srand(1567);        
 
       for (i = 0; i < 30; i++)
       {
@@ -117,16 +124,11 @@ LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
     {
       INT i, j;
 
-      for (i = r + 1; i < 2000; i += (2 * r + 10))
-        for (j = r + 1; j < 1200; j += (2 * r + 10))
-          DrawEye(hMemDC, i, j, r, r / 5, pt.x, pt.y);
+      for (i = 50 + 1; i < 2000; i += (2 * 50 + 10))
+        for (j = 50 + 1; j < 1200; j += (2 * 50 + 10))
+          DrawEye(hMemDC, i, j, 50, 20, pt.x, pt.y);
     }
-    
-    InvalidateRect(hWnd, NULL, FALSE);
 
-    return 0;
-  case WM_PAINT:
-    hDC = BeginPaint(hWnd, &ps);
     BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
     EndPaint(hWnd, &ps);
     return 0;
